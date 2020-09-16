37cf23882b4dc6b7c377411f5786461c87e7ee7c
acan <779731690@qq.com>
2020-09-16 23:45:49 +0800

    完成了主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,20 @@
 
 $.ajaxPrefilter(function(options) {
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url);
+
+    // 给有权限的接口统一设置请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    options.complete = function(res) {
+        // console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+
+    }
 })
